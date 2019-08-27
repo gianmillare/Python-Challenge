10d9d0be85b5b66c4c9b5d67c8cbefab827d9331
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2019-08-26 17:56:04 -0700

    starting code

---
+++ PyBank/Main.py
@@ -0,0 +1,20 @@
+# Initial Set Up
+import os
+import csv
+
+# Setting Variables
+
+ # Total Number of months
+    tmonth = 0
+ # Total net amount of Profit/Losses  
+    tnet = 0
+ # Average of Profit/Losses
+    aver = 0
+ # Greatest increase in profits using Date and Amount
+    gincrease = 0
+    gincreasemonth = 0
+
+ # Greatest decrease in profits using Data and Amount
+    gdecrease = 0
+    gdecreasemonth = 0
+

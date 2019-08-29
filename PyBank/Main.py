af40c671f35cfaf35ef5c409894fabbb87d78a38
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2019-08-28 23:13:41 -0700

    fixed errors

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -5,9 +5,10 @@ import csv
 csvpath = os.path.join('Resources','budget_data.csv')
 
 # Setting Variables
+nextmonth = []
+monthcount = []
 tmonth = 0
 tnet = 0
-average = 0
 gincrease = 0
 gincreasemonth = 0
 gdecrease = 0
@@ -23,36 +24,36 @@ with open(csvpath, newline="") as csvfile:
     csvheader = next(csvreader)
     row = next(csvreader)
 
-    # Calculating initial variables
+    # Setting initial variables
     previousrow = int(row[1])
     tmonth = tmonth + 1
     tnet = tnet + int(row[1])
     gincrease = int(row[1])
-    gincreasemonth = str(row[0])
+    gincreasemonth = row[0]
+    gdecrease = int(row[1])
+    gdecreasemonth = row[0]
 
     for row in csvreader:
-    
+
+        # Calculate total number of months
+        tmonth = tmonth + 1
+        # Calculate the net of Profit/Losses
+        tnet = tnet + int(row[1])
+        
         # Begin reading each row
         revenuechange = int(row[1]) - previousrow
-        nextmonth = []
         nextmonth.append(revenuechange)
         previousrow = int(row[1])
-        monthcount = []
         monthcount.append(row[0])
-
-       # Calculate total number of months
-        tmonth = tmonth + 1
-       # Calculate the net of Profit/Losses
-        tnet = tnet + int(row[1])
        
        #Set the greatest increase and decrease
-    if int(row[1]) > gincrease:
-        gincrease = int(row[1])
-        gincreasemonth = int(row[0])
+        if int(row[1]) > gincrease:
+            gincrease = int(row[1])
+            gincreasemonth = row[0]
 
-    if int(row[1]) < gdecrease:
-        gdecrease = int(row[1])
-        gdecreasemonth = int(row[0])
+        if int(row[1]) < gdecrease:
+            gdecrease = int(row[1])
+            gdecreasemonth = row[0]
             
     # Find the average of monthly revenue changes
     average = sum(nextmonth)/len(nextmonth)
@@ -63,10 +64,11 @@ with open(csvpath, newline="") as csvfile:
 # Analysis
 print(f"Financial Analysis")
 print(f"------------------")
-print(f"Total Months = tmonth")
-print(f"Average Change = average")
-print(f"Greatest Increase in Profits = gincreasemonth revenuemax")
-print(f"Greates Decrease in Profits = gdecreasemonth revenuemin")
+print(f"Total Months = {tmonth}")
+print(f"Total = ${tnet}")
+print(f"Average Change = ${average}")
+print(f"Greatest Increase in Profits = {gincreasemonth} (${revenuemax})")
+print(f"Greatest Decrease in Profits = {gdecreasemonth} (${revenuemin})")
 
     
                          
